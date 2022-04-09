cbaff7b59658bc907a4443991da0af24817821f7
jyotijauhari <jyotijauhari222@gmail.com>
2022-04-10 02:29:28 +0530

    help command done

--- commands/help.js
+++ commands/help.js
@@ -1,7 +1,15 @@
 //for help command
 
 function help(){
+    console.log(
+        `
+        These different commands supported by this "file organiser" project are:
 
+        1. node main.js tree <path>
+        2. node main.js organize <path>
+        3. node main.js help
+        `
+    );
 }
 
 module.exports = {
